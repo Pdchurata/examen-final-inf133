bd6dae23dff15567880624768eb0faee405b974d
PABEL DANTE CHURATA APAZA <107067781+Pdchurata@users.noreply.github.com>
2024-06-12 13:50:05 -0400

    Delete EntregaFinal/app directory

--- EntregaFinal/app/controllers/_init_.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('main', __name__)
-
-from app.controllers import (
-    restaurant_controller,
-    reservation_controller,
-    user_controller
-)
--- EntregaFinal/app/models/_init_.py
+++
@@ -1,3 +0,0 @@
-from app.models.restaurant import Restaurant
-from app.models.reservation import Reservation
-from app.models.user import User
